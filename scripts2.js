7f82438a4a817ce050e6fd6ca7c35620cba0d283
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2024-10-01 12:51:09 -0400

    edit

--- scripts2.js
+++ scripts2.js
@@ -44,7 +44,7 @@ function weatherShowFn(data,data2) {
 }
 
 	let locationIcon = document.querySelector('#weather-icon');
-	const {icon} = data.weather[0].icon;
+	const {icon} = data.weather[0];
 	locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
 
 	
