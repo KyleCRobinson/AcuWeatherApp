226800f7cb034afe2172b44035e57868101b32ba
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2024-10-01 03:04:30 -0400

    edt

--- scripts2.js
+++ scripts2.js
@@ -1,4 +1,5 @@
 const url ='https://api.openweathermap.org/data/2.5/weather';
+const url2 = 'https://openweathermap.org/img/wn/10d@2x.png';
 const apiKey ='f00c38e0279b7bc85480c3fe775d518c';
 
 $(document).ready(function () {
